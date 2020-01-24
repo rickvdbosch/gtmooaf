5dee79e34442e7f63fb46027bf6163b5dc704a03
Rick van den Bosch <rickvdbosch@outlook.com>
2020-01-24 15:35:33 +0100

    Aded TableInput example

---
+++ gtmooaf/Models/Tent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Gtmooaf.Models
+{
+    public class Tent : TableEntity
+    {
+        #region
+
+        public Tent() : base("azurelowlands", DateTime.Now.ToString("yyyyMMddHHmm"))
+        {
+        }
+
+        #endregion
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ gtmooaf/TriggersBindings/TableInput.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+
+using Gtmooaf.Models;
+
+namespace Gtmooaf.TriggersBindings
+{
+    public static class TableInput
+    {
+        [FunctionName(nameof(TableInput))]
+        public static async Task Run(
+            [QueueTrigger("trigger", Connection = "scs")] string message,
+            [Table(nameof(Tent))] CloudTable table,
+            ILogger log)
+        {
+            var entities = await GetEntitiesFromTable(table);
+            log.LogInformation($"The Function was triggered based on this message on the queue: {message}");
+
+            foreach (var entity in entities)
+            {
+                log.LogInformation($"{entity.Id}:\t{entity.Name}");
+
+            }
+        }
+
+        #region Private methods
+
+        private static async Task<List<Tent>> GetEntitiesFromTable(CloudTable table)
+        {
+            TableQuerySegment<Tent> querySegment = null;
+            var entities = new List<Tent>();
+            var query = new TableQuery<Tent>().Where(TableQuery.GenerateFilterCondition(nameof(ITableEntity.PartitionKey), QueryComparisons.Equal, "azurelowlands"));
+
+            do
+            {
+                querySegment = await table.ExecuteQuerySegmentedAsync(query, querySegment?.ContinuationToken);
+                entities.AddRange(querySegment.Results);
+            } while (querySegment.ContinuationToken != null);
+
+            return entities;
+        }
+
+        #endregion
+    }
+}
